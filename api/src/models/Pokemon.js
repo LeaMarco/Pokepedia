457c90e9262eb78d66b318751a9150a05719f68a
Lea <mmleandro@live.com.ar>
2021-06-05 19:04:15 -0300

    primer push backend

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -3,10 +3,41 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('pokemon', {
+  sequelize.define('Pokemon', {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    Hp: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    Attack: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    Defense: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    Speed: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    Height: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    Speed: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },{initialAutoIncrement: 2000});
+  
 };
