567dee250ef3a1fd8a8bb3cf1d9fdc240db896bf
Lea <mmleandro@live.com.ar>
2021-08-02 15:38:47 -0300

    cambio en actions con variables a .env

--- api/src/app.js
+++ api/src/app.js
@@ -1,35 +1,45 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const routes = require('./routes/index');
+const routes = require('./routes/index.js');
+const cors = require('cors')
 
-require('./db/index');
+require('./db.js');
 
 const server = express();
+server.use(cors())
 
 server.name = 'API';
+
 
 server.use(express.urlencoded({ extended: true, limit: '50mb' }));
 server.use(express.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
-server.use((req, res, next) => {
-  res.header(
-    'Access-Control-Allow-Origin',
-    process.env.HOST_FRONT
-  );
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  res.header(
-    'Access-Control-Allow-Methods',
-    'GET, POST, OPTIONS, PATCH, DELETE'
-  );
-  next();
-});
+
+
+
+
+// server.use('/', express.static(path.join(__dirname, '/')));
+
+// server.use(
+// 	cors({
+// 		origin: process.env.HOST_FRONT,
+// 		credentials: false,
+// 		methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'DELETE'],
+// 		allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
+// 	})
+// );
 
 server.use('/', routes);
+
+// Error catching endware.
+server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+  const status = err.status || 500;
+  const message = err.message || err;
+  console.error(err);
+  res.status(status).send(message);
+});
 
 module.exports = server;
