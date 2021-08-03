d6ec9787c56b4b9d0775d837fe2986ad0632064f
Lea <mmleandro@live.com.ar>
2021-08-03 16:29:28 -0300

    cambio en el index

--- api/src/db.js
+++ api/src/db.js
@@ -9,6 +9,12 @@ const {
 const sequelize = new Sequelize(`${process.env.DATABASE_URL}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
 });
 const basename = path.basename(__filename);
 
